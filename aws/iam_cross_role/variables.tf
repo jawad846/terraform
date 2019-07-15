b2a91f69fd2cd6e60f17d0ddc3c4e0c9a9da423b
jawad846 <jawad846@gmail.com>
2019-07-15 15:21:17 -0700

    Added iam cross role

---
+++ aws/iam_cross_role/variables.tf
@@ -0,0 +1,17 @@
+variable "profile" {
+  description = "AWS Profile"
+  default     = "default"
+}
+
+variable "role_name" {
+  description = "Name of role"
+}
+
+variable "permitted_account_id" {
+  description = "Account ID of AWS account which gets the permission to the role"
+}
+
+variable "role_policy_arn" {
+  description = "Policy ARN for assume role"
+  default     = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
